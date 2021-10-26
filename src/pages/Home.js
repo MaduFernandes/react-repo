8349364c54043525257d5c6a2bbe440dcbb42f48
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-10-25 21:59:38 -0300

    Add input field

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <section className="Input">
+        <input type="text" id="repository" name="repository" placeholder="Repositório" />
+      </section>
+    </>
+  )
+}
+
+export default Home
